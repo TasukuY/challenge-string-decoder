4f2c7feeebbc8564217210c429870847964c8712
TYama <alohaty81@gmail.com>
2022-02-22 18:08:04 -0700

    coding challenge done

---
+++ stringDecoder.js
@@ -0,0 +1,30 @@
+let decoder = code => {
+    let decodedWord = '';
+    
+    for(let i = 0; i < code.length; i++){
+        //if the code[i] is a number
+        if(!(isNaN(code[i]))){
+            //set the skipNum
+            let skipNum = parseInt(code[i]) + 1;
+            decodedWord += code[i + skipNum];
+        }
+    }
+    return decodedWord;
+}
+
+//console.log(decoder('0h2xce5ngbrdy'));
+
+let decoder2 = code => {
+    let decodedWord = '';
+    for(let i = 1; i < code.length; i++){
+        //if the code[i] is a number
+        if(!(isNaN(code[i]))){
+            //set the skipNum
+            decodedWord += code[i - 1];
+        }
+    }
+    decodedWord += code[code.length - 1];
+    return decodedWord;
+}
+
+console.log(decoder2('0y4akjfe0s'));
